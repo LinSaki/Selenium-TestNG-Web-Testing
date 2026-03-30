c53443337e5313768e34efa9d6dd0a48129bd976
LinSaki <saquika@sheridancollege.ca>
2026-03-29 21:44:51 -0400

    completed ProductsTest script

--- src/test/java/part2/com/saucedemo/tests/products/ProductsTest.java
+++ src/test/java/part2/com/saucedemo/tests/products/ProductsTest.java
@@ -1,5 +1,16 @@
 package part2.com.saucedemo.tests.products;
 
-public class ProductsTest {
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
+import com.saucedemo.pages.ProductsPage;
+
+import part2.com.saucedemo.base.BaseTest;
+
+public class ProductsTest extends BaseTest {
+	@Test
+	public void testProductsHeaderIsDisplayed() { //use convenience method
+		ProductsPage productsPage = loginPage.logIntoApplication("standard_user", "secret_sauce");
+	Assert.assertTrue(productsPage.isProductsHeaderDIsplayed(), "\n Products Header is NOT displayed \n ");
+	}
 }

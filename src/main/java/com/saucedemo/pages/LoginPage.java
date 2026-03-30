bc6dd25e65896fd19fafc385a04a7514c1bd0e5c
LinSaki <saquika@sheridancollege.ca>
2026-03-29 20:50:35 -0400

    defined LoginPage methods, setters and transition

--- src/main/java/com/saucedemo/pages/LoginPage.java
+++ src/main/java/com/saucedemo/pages/LoginPage.java
@@ -6,5 +6,18 @@ public class LoginPage extends BasePage {
 	private By usernameField = By.id("user-name");
 	private By passwordField = By.id("password");
 	private By loginButton = By.id("login-button");
-	private By errorMessage = By.xpath("#login_button_container h3");
+	private By errorMessage = By.xpath("#login_button_container h3"); //XPath (XML Path Language) is an expression and query language developed by the W3C 
+																	  //used to navigate, select, and manipulate nodes (elements, attributes, text, etc.) 
+																	  //in XML and HTML documents.
+	public void setUsername(String username) {
+		set(usernameField,username);
+	}
+	
+	public void setPassword(String password) {
+		set(passwordField, password);
+	}
+	
+	public void clickLoginButton() { //transition method, should NOT be void, it redirects you somewhere
+		click(loginButton);
+	}
 }

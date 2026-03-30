e4a98b60e507be6c722c8eff4a1fa91c52f8e63a
LinSaki <saquika@sheridancollege.ca>
2026-03-29 21:36:54 -0400

    completed method for LoginTests script

--- src/main/java/com/saucedemo/pages/LoginPage.java
+++ src/main/java/com/saucedemo/pages/LoginPage.java
@@ -6,7 +6,7 @@ public class LoginPage extends BasePage {
 	private By usernameField = By.id("user-name");
 	private By passwordField = By.id("password");
 	private By loginButton = By.id("login-button");
-	private By errorMessage = By.xpath("#login_button_container h3"); //XPath (XML Path Language) is an expression and query language developed by the W3C 
+	private By errorMessage = By.cssSelector("#login_button_container h3"); //XPath (XML Path Language) is an expression and query language developed by the W3C 
 																	  //used to navigate, select, and manipulate nodes (elements, attributes, text, etc.) 
 																	  //in XML and HTML documents.
 	public void setUsername(String username) {
--- src/test/java/part2/com/saucedemo/tests/login/LoginTests.java
+++ src/test/java/part2/com/saucedemo/tests/login/LoginTests.java
@@ -1,7 +1,17 @@
 package part2.com.saucedemo.tests.login;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
 import part2.com.saucedemo.base.BaseTest;
 
 public class LoginTests extends BaseTest {
-
+	
+	@Test
+	public void testLoginErrorMessage() { //test if error message shows from Login Page when invalid password is entered
+		loginPage.setUsername("standard_user");
+		loginPage.setPassword("abc123"); //wrong password
+		loginPage.clickLoginButton();
+		String actualMessage = loginPage.getErrorMessage();
+		Assert.assertTrue(actualMessage.contains("Epic sadface:"));
+	}
 }
